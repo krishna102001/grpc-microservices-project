beaf0b340c36c90ed1c6daffd603562e80035af1
krishna102001 <krishnakantmaurya@icloud.com>
2025-09-08 17:28:41 +0530

    db model added

--- auth-service/go.mod
+++ auth-service/go.mod
@@ -5,6 +5,7 @@ go 1.24.0
 require (
 	github.com/charmbracelet/log v0.4.2
 	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.41.0
 	google.golang.org/grpc v1.75.0
--- auth-service/models/model.go
+++ auth-service/models/model.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `gorm:"primaryKey"`
+	Name      string    `gorm:"not null"`
+	Email     string    `gorm:"uniqueIndex;not null"`
+	Password  string    `gorm:"not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
