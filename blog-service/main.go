107eb196b1a4b252d4e5f3909738f9a85c0db9db
krishna102001 <krishnakantmaurya@icloud.com>
2025-09-08 22:17:33 +0530

    gRPC server started

--- blog-service/main.go
+++ blog-service/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"log"
+	"net"
+	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/krishna102001/grpc-microservices-project/blog-service/database"
+	"github.com/krishna102001/grpc-microservices-project/blog-service/pb"
+	"github.com/krishna102001/grpc-microservices-project/blog-service/service"
+	"google.golang.org/grpc"
 )
 
 func init() {
@@ -15,5 +20,21 @@ func init() {
 }
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Env value not found")
+	}
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("Failed to connect to tcp server")
+	}
 
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterBlog_ServiceServer(grpcServer, &service.BlogServer{})
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start the gRPC Server")
+	}
+	log.Printf("gRPC server started of Blog Service on port on %v", lis.Addr())
 }
--- blog-service/service/service.go
+++ blog-service/service/service.go
@@ -1,1 +1,7 @@
 package service
+
+import "github.com/krishna102001/grpc-microservices-project/blog-service/pb"
+
+type BlogServer struct {
+	pb.UnimplementedBlog_ServiceServer
+}
